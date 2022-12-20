97f4ae16661a848be5d879554387a826c0f0b7cf
Thiago Lima <thenriq@gmail.com>
2022-12-20 01:23:12 +0000

    set game over status by limiting new attempts to 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
             if (this.getAttribute("data-type") === "submit") {
                 //alert("You clicked Submit!");
                 runGame();
-                incrementDiv();
+                //incrementDiv();
                 
             } else {
                 let option = this.getAttribute("data-type");
@@ -29,7 +29,16 @@ document.addEventListener("DOMContentLoaded", function(){
 function runGame() {
     let secretPin = Array.from({length: 4}, () => Math.floor(Math.random() * 10));
     alert(secretPin);
-      
+
+    let lastDiv = document.getElementById('guessing-area').lastChild;
+    let lastDivID = parseInt(lastDiv.id);
+    if ((lastDivID) < 6) {
+        incrementDiv();
+    } else {
+        alert("game over. Page will refresh.");
+        alert(lastDivID);
+        window.location.reload();
+    }
 }
 
 function createDiv() {
@@ -51,15 +60,17 @@ function incrementDiv() {
     const container = document.getElementById('guessing-area');
     let lastDiv = document.getElementById('guessing-area').lastChild;
     let lastDivID = parseInt(lastDiv.id);
-    container.innerHTML+=`
-        <div class="guessing-row" id="${lastDivID + 1}">
-            <form action="">
-                <input type="text" class="form-num" id="num1" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');"/>
-                <input type="text" class="form-num" id="num2" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');"/>
-                <input type="text" class="form-num" id="num3" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');"/>
-                <input type="text" class="form-num" id="num4" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');"/>
-            </form>
-        </div>`;
+    
+        container.innerHTML+=`
+            <div class="guessing-row" id="${lastDivID + 1}">
+                <form action="">
+                    <input type="text" class="form-num" id="num1" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');"/>
+                    <input type="text" class="form-num" id="num2" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');"/>
+                    <input type="text" class="form-num" id="num3" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');"/>
+                    <input type="text" class="form-num" id="num4" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');"/>
+                </form>
+            </div>`;
+    
     autoTab();  
 }
 
@@ -76,8 +87,7 @@ function autoTab() {
         
     });
 
-    
-    
 }
+
 
 
